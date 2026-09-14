7df1bb146bf0b0f498ff022ab673fccbe8120397
stoneman-mls <160383637+stoneman-mls@users.noreply.github.com>
2026-09-14 04:09:55 +0000

    updates

--- home.py
+++ home.py
@@ -1,11 +1,24 @@
+## these are libraries that are being imported
 import streamlit as st
+from openai import OpenAI
 
+# this is the title of the webpage
 st.title("LAWS90286")
 
+# here is some user input
 name = st.text_input("What is your name?")
 age = st.number_input("What is your age?", min_value=0)
 
+# here is some logic to say hello
 if st.button("Say hi"):
     st.write(f"Hello, {name}. I understand you are {age} years old.")
 
-st.write("a change")
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4o",
+    input="Write a one-sentence bedtime story about a unicorn.",
+)
+
+st.write(response.output_text)
